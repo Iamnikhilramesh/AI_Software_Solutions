9284eeb411cd2de28b35bcd4df6ef59c657cdac9
Nikhil Git <iamnikhilramesh@gmail.com>
2021-03-30 12:21:08 +0200

    unknown

--- app.py
+++ app.py
@@ -26,9 +26,7 @@ st.sidebar.markdown(""" ## Address & Contact Info
 #### 98 Kärrhöksgatan,55612 Sweden  
 #### _ :email: iamnikhilramesh@gamil.com _
 #### _ :phone: +46 - 764439519 _
-#### _ [Instagram](https://www.instagram.com/iamnikhilramesh/) _
-#### _ [Linkedin](https://www.linkedin.com/in/nikhil-ramesh-5125b7139/) _
-#### _ [Github](https://github.com/Iamnikhilramesh) _
+#### _ [Instagram](https://www.instagram.com/iamnikhilramesh/)  [Linkedin](https://www.linkedin.com/in/nikhil-ramesh-5125b7139/)  [Github](https://github.com/Iamnikhilramesh) _
 
  """)
 
@@ -36,28 +34,79 @@ st.sidebar.markdown(""" ## Address & Contact Info
 def mail():
     st.header("Need Assistance ???")
     st.title("Let's work together")
-    service = st.selectbox("Select Required Service",['Data Application','Machine Learning Application','ETL Solution',"Artificial Intelligence Application","Others"])
-    name = st.text_input("Your Name")
-    email = st.text_input("Your Email")
-    message = st.text_input("Your Message")
+    name,email = st.beta_columns([6,13])
+    service, message = st.beta_columns([6,10])
+    name = name.text_input("Your Name")
+    service = service.selectbox("Select Required Service",['Data Application','Machine Learning Application','ETL Solution',"Artificial Intelligence Application","Others"])
+    email = email.text_input("Your Email")
+    message = message.text_input("Your Message")
     return name,email,message,service
 
 #Function to display home page
 def home():
     st.header("Home")
+
+    mission = st.beta_expander("Our Mission and Goal ", expanded=True)
+    with mission:
+        st.text("Our Mission and Goal")
+    
+    values = st.beta_expander("Core Values ", expanded=True)
+    with values:
+        st.text("Core Values")
+    
+    principles = st.beta_expander("Core Principles ", expanded=True)
+    with principles:
+        st.text("Core Principles")
+    
+    my_expander = st.beta_expander("Some of the Clients I have worked with", expanded=True)
+    with my_expander:
+        d,e = st.beta_columns(2)
+        d.image("isha.jpeg",caption="Isha Foundations")
+        e.image("nccab.png",caption="NCC AB")
+        f,g = st.beta_columns(2)
+        f.image("kmc.jpeg",caption="Kaveri Medical Center")
+        g.image("luxin.png",caption="Luxin Group AB")
+        a,b,c = st.beta_columns(3)
+        a.image("agile.png", caption="Agile Health")
+        b.image("aj.png",caption="A J Hospital")
+        c.image("employchain.jpeg",caption="Employchain AB")
+    
+    team = st.beta_expander("Team ", expanded=True)
+    with team:
+        st.text("Team")
+        st.markdown("""#### [CEO](upload://isha.jpeg) [Linkedin](https://www.linkedin.com/in/nikhil-ramesh-5125b7139/) [Github](https://github.com/Iamnikhilramesh) """)
+    
+    events = st.beta_expander("Events ", expanded=True)
+    with events:
+        st.text("Events")
+        
 #Function to display projects
 def projects():
     my_expander_p = st.beta_expander("Some of the Projects I have worked with", expanded=True)
-    with my_expander_p: 
-        st.image("leaf.jpeg",caption="Leaf Feature Extraction (2017)")
-        st.image("Hospital.jpeg", caption="Hospital managament System (2018)")
-        st.image("LIS.png",caption="Lab Information System and Radiology Information System (2018)")
-        st.image("allergy.jpeg",caption="Allergen Identification(2019)")
-        st.image("doctor.jpeg",caption="Doctor Appointment Application (2019)")
-        st.image("jobmatch.jpeg",caption="Job Matching App (2020)")
-        st.image("lane.jpeg",caption="Lane Detection for self driving car (2020)")
-        st.image("treeclasify.jpeg",caption="Tree classification using CNN (2020)")
-        st.image("bim.jpeg",caption="BIM Class Detection Bot (2021)")
+    with my_expander_p:
+        a,b = st.beta_columns(2)
+        a.image("leaf.jpeg",caption="Leaf Feature Extraction (2017)")
+        a.markdown("""[Click here to know more](https://www.youtube.com/)""")
+        b.image("Hospital.jpeg", caption="Hospital managament System (2018)")
+        b.markdown("""[Click here to know more](https://www.youtube.com/)""")
+        c,d = st.beta_columns(2)
+        c.image("LIS.png",caption="Lab Information System and Radiology Information System (2018)")
+        c.markdown("""[Click here to know more](https://www.youtube.com/)""")
+        d.image("allergy.jpeg",caption="Allergen Identification(2019)")
+        d.markdown("""[Click here to know more](https://www.youtube.com/)""")
+        e,f = st.beta_columns(2)
+        e.image("doctor.jpeg",caption="Doctor Appointment Application (2019)")
+        e.markdown("""[Click here to know more](https://www.youtube.com/)""")
+        f.image("jobmatch.jpeg",caption="Job Matching App (2020)")
+        f.markdown("""[Click here to know more](https://www.youtube.com/)""")
+        g,h = st.beta_columns(2)
+        g.image("lane.jpeg",caption="Lane Detection for self driving car (2020)")
+        g.markdown("""[Click here to know more](https://www.youtube.com/)""")
+        h.image("treeclasify.jpeg",caption="Tree classification using CNN (2020)")
+        h.markdown("""[Click here to know more](https://www.youtube.com/)""")
+        i, j= st.beta_columns(2)
+        i.image("bim.jpeg",caption="BIM Class Detection Bot (2021)")
+        i.markdown("""[Click here to know more](https://www.youtube.com/)""")
 
 
 
